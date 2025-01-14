4d958c71aebf5a2cb7afa075d66301be00ba09fa
Priti Dawande <kulkarni.prity@gmail.com>
2025-01-14 16:06:25 +0530

    for loop with end limit by user

---
+++ forloop2ud.sql
@@ -0,0 +1,12 @@
+--write a programe to display for loop by using end limit user-defined
+set serveroutput on;
+declare
+    i number;
+    n number;
+begin
+    n:=&no;
+    for i in 1..n loop
+        DBMS_OUTPUT.PUT_LINE(i);
+    end loop;
+end;
+/   
