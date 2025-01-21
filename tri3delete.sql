d8012edb4fbad05f6fb0c0fd4c01f7ee8be2c3fb
Priti Dawande <kulkarni.prity@gmail.com>
2025-01-21 15:09:02 +0530

    trigger delete

---
+++ tri3delete.sql
@@ -0,0 +1,12 @@
+set SERVEROUTPUT ON;
+CREATE OR REPLACE TRIGGER bu_Superheroes
+BEFORE DELETE ON superheroes
+FOR EACH ROW
+ENABLE
+DECLARE
+  v_user  VARCHAR2 (15);
+BEGIN
+ SELECT user INTO v_user FROM dual;
+ DBMS_OUTPUT.PUT_LINE('You Just Deleted a Row Mr.'|| v_user); 
+END;
+/
