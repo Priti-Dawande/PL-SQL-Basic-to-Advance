f8e67689462a8159f5ab764b76fb409941db3023
Priti Dawande <kulkarni.prity@gmail.com>
2025-01-21 15:08:40 +0530

    trigger update

---
+++ tri2update.sql
@@ -0,0 +1,12 @@
+set SERVEROUTPUT ON;
+CREATE OR REPLACE TRIGGER bu_Superheroes
+BEFORE UPDATE ON superheroes
+FOR EACH ROW
+ENABLE
+DECLARE
+  v_user  VARCHAR2 (15);
+BEGIN
+ SELECT user INTO v_user FROM dual;
+ DBMS_OUTPUT.PUT_LINE('You Just Updated a Row Mr.'|| v_user); 
+END;
+/
